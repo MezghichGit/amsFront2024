655e85d78e55c722b3d790a1d9e3f5a1d66ef73e
moahmed amine mezghich <git@mezghich.com>
2024-01-07 15:44:53 +0100

    correction exo n°1

--- src/app/stagiaire/stagiaire.component.ts
+++ src/app/stagiaire/stagiaire.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FirstLetterUpperCasePipe } from "../first-letter-upper-case.pipe";
 
 @Component({
-  selector: 'app-stagiaire',
-  standalone: true,
-  imports: [],
-  templateUrl: './stagiaire.component.html',
-  styleUrl: './stagiaire.component.css'
+    selector: 'app-stagiaire',
+    standalone: true,
+    templateUrl: './stagiaire.component.html',
+    styleUrl: './stagiaire.component.css',
+    imports: [CommonModule, FirstLetterUpperCasePipe]
 })
 export class StagiaireComponent {
+  formation: string = "Angular";
+  formateur : string = "Dr. Mohamed Amine MEZGHICH";
+  stagiaires : string[]=["sofien","marwa","hakim","borel","fares","atef"];
 
 }
