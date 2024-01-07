bc074ccd532e5cb8d93fda3f95abddd2546a452a
moahmed amine mezghich <git@mezghich.com>
2024-01-07 17:13:42 +0100

    correction exo 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,19 @@ import { RouterOutlet } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { StagiaireComponent } from './stagiaire/stagiaire.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PostComponent } from './post/post.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContactComponent, StagiaireComponent, HttpClientModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ContactComponent,
+    StagiaireComponent,
+    PostComponent,
+    HttpClientModule
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
